b5a72809b726dea61426498ef39b315e4acf930e
SKeana <Skean2000@outlook.com>
2025-03-22 14:58:50 +0000

    added css and planningBox. Work on app.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,25 @@ function App() {
   return (
     <div className="app-container">
       <header className="app-header">
-        <h1>Task Management App</h1>
+        <h1>Task Management</h1>
       </header>
       <main className="app-main">
         <PlanningBox />
-        {/* Add other components here (TaskBoard, TaskForm, etc.) */}
+        {/* Add other components here (TaskBoard, TaskForm, etc.) */
+          <div className='columns-planning'>
+            <h2>Task that need to be done</h2>
+          </div>
+        }
+        
+        <div className='columns-working'>
+          <h2>Task that need more work</h2>
+        </div>
+        <div className='columns-done'>
+          <h2>Tasks that are done</h2>
+        </div>
       </main>
     </div>
   );
 }
 
 export default App;
-
